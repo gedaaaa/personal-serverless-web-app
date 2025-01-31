c16f4d08c6c4ab12961e03fc6283e27cb401f89f
gedaaaa <buaa38270206@gmail.com>
2025-01-31 17:29:44 +0800

    init commit

---
+++ apps/frontend/todo-list-website/vite.config.ts
@@ -0,0 +1,44 @@
+/// <reference types='vitest' />
+import { defineConfig } from 'vite';
+import { sveltekit } from '@sveltejs/kit/vite';
+import { nxViteTsPaths } from '@nx/vite/plugins/nx-tsconfig-paths.plugin';
+import { nxCopyAssetsPlugin } from '@nx/vite/plugins/nx-copy-assets.plugin';
+
+const sveltekitFix = async () => {
+  const cwd = process.cwd();
+  process.chdir(__dirname); // Temporarily change the working directory
+  const plugin = await sveltekit(); // Load the SvelteKit plugin
+  process.chdir(cwd); // Restore the original working directory
+  return plugin;
+};
+
+export default defineConfig({
+  root: __dirname,
+  cacheDir: '../../../node_modules/.vite/todo-list-website',
+  server: {
+    port: 4200,
+    host: 'localhost',
+    fs: {
+      allow: [
+        // your custom rules
+        '../../../node_modules',
+      ],
+    },
+  },
+  preview: {
+    port: 4300,
+    host: 'localhost',
+  },
+  plugins: [sveltekitFix(), nxViteTsPaths(), nxCopyAssetsPlugin(['*.md'])],
+  // Uncomment this if you are using workers.
+  // worker: {
+  //  plugins: [ nxViteTsPaths() ],
+  // },
+  build: {
+    emptyOutDir: true,
+    reportCompressedSize: true,
+    commonjsOptions: {
+      transformMixedEsModules: true,
+    },
+  },
+});

b892ae734975b2df4dd49c882c47c78ab105946d
Depeng Meng <30386199+gedaaaa@users.noreply.github.com>
2025-02-03 18:05:18 +0800

    chore: extract gradle to root, support shared lib (#22)

---
+++ libs/jvm-shared-lib/build.gradle.kts
@@ -0,0 +1,33 @@
+import org.jlleitschuh.gradle.ktlint.reporter.ReporterType
+
+plugins {
+    id("org.jetbrains.kotlin.jvm") version "1.9.25"
+    id("org.jetbrains.kotlin.plugin.allopen") version "1.9.25"
+    id("org.jlleitschuh.gradle.ktlint") version "12.1.2"
+}
+
+ktlint {
+    version.set("1.5.0")
+    android.set(false)
+    outputToConsole.set(true)
+    outputColorName.set("RED")
+    reporters {
+        reporter(ReporterType.CHECKSTYLE)
+        reporter(ReporterType.HTML)
+    }
+}
+tasks.withType<org.jlleitschuh.gradle.ktlint.tasks.KtLintCheckTask>().configureEach {
+    enabled = true
+}
+
+tasks.withType<org.jlleitschuh.gradle.ktlint.tasks.KtLintFormatTask>().configureEach {
+    enabled = true
+}
+
+version = "0.1"
+group = "top.sunbath.shared"
+
+repositories {
+    mavenCentral()
+}
+dependencies {}

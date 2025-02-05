92f89f3b1a50810282113037464aa81e661e8a69
Depeng Meng <30386199+gedaaaa@users.noreply.github.com>
2025-02-05 15:33:29 +0800

    chore(api): use http api instead of rest api (#38)

--- apps/backend/helloworld/infra/src/test/java/top/sunbath/api/AppStackTest.java
+++ apps/backend/helloworld/infra/src/test/java/top/sunbath/api/AppStackTest.java
@@ -13,7 +13,7 @@ class AppStackTest {
         if (new File(AppStack.functionPath()).exists()) {
             AppStack stack = new AppStack(new App(), "TestMicronautAppStack");
             Template template = Template.fromStack(stack);
-            template.hasResourceProperties("AWS::Lambda::Function", Collections.singletonMap("Handler", "io.micronaut.function.aws.proxy.payload1.ApiGatewayProxyRequestEventFunction"));
+            template.hasResourceProperties("AWS::Lambda::Function", Collections.singletonMap("Handler", "io.micronaut.function.aws.proxy.payload2.APIGatewayV2HTTPEventFunction"));
         }
     }
 }

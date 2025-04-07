87feef157359593a08151dfccb55e02cf15622f3
Depeng Meng <buaa38270206@gmail.com>
2025-04-07 06:41:02 +0000

    feat(auth): add dependencies for shared library and enhance AppStack with SSM Parameter Store access

--- apps/backend/auth/infra/src/main/java/top/sunbath/api/auth/AppStack.java
+++ apps/backend/auth/infra/src/main/java/top/sunbath/api/auth/AppStack.java
@@ -24,103 +24,119 @@ import java.util.Map;
 
 public class AppStack extends Stack {
 
-    public AppStack(final Construct parent, final String id) {
-        this(parent, id, null);
-    }
+        public AppStack(final Construct parent, final String id) {
+                this(parent, id, null);
+        }
 
-    public AppStack(final Construct parent, final String id, final StackProps props) {
-        super(parent, id, props);
+        public AppStack(final Construct parent, final String id, final StackProps props) {
+                super(parent, id, props);
 
-        var serviceName = "auth";
-        var dynamodbSingleTableName = "auth-service-single-table";
-        var dynamodbDistributedLocksTableName = "distributed_locks";
+                var serviceName = "auth";
+                var dynamodbSingleTableName = "auth-service-single-table";
+                var dynamodbDistributedLocksTableName = "distributed_locks";
 
-        Map<String, String> environmentVariables = new HashMap<>();
-        // 设置生产环境
-        environmentVariables.put("MICRONAUT_ENVIRONMENTS", "production");
-        // 设置 DynamoDB 表名
-        environmentVariables.put("DYNAMODB_TABLE_NAME", dynamodbSingleTableName);
+                Map<String, String> environmentVariables = new HashMap<>();
+                // 设置生产环境
+                environmentVariables.put("MICRONAUT_ENVIRONMENTS", "production");
+                // 设置 DynamoDB 表名
+                environmentVariables.put("DYNAMODB_TABLE_NAME", dynamodbSingleTableName);
 
-        var function = MicronautFunction.create(ApplicationType.DEFAULT, false, this, serviceName + "-function")
-                .runtime(Runtime.JAVA_21)
-                .handler("io.micronaut.function.aws.proxy.payload2.APIGatewayV2HTTPEventFunction")
-                .environment(environmentVariables).code(Code.fromAsset(functionPath())).timeout(Duration.seconds(10))
-                .memorySize(512).logRetention(RetentionDays.ONE_WEEK).tracing(Tracing.ACTIVE)
-                .architecture(Architecture.X86_64).snapStart(SnapStartConf.ON_PUBLISHED_VERSIONS).build();
+                var function = MicronautFunction.create(ApplicationType.DEFAULT, false, this, serviceName + "-function")
+                                .runtime(Runtime.JAVA_21)
+                                .handler("io.micronaut.function.aws.proxy.payload2.APIGatewayV2HTTPEventFunction")
+                                .environment(environmentVariables).code(Code.fromAsset(functionPath()))
+                                .timeout(Duration.seconds(10)).memorySize(512).logRetention(RetentionDays.ONE_WEEK)
+                                .tracing(Tracing.ACTIVE).architecture(Architecture.X86_64)
+                                .snapStart(SnapStartConf.ON_PUBLISHED_VERSIONS).build();
 
-        // 获取当前区域和账户 ID
-        String region = this.getRegion();
-        String accountId = this.getAccount();
+                // 获取当前区域和账户 ID
+                String region = this.getRegion();
+                String accountId = this.getAccount();
 
-        // 构建分布式锁表的 ARN（包含账户 ID）
-        String distributedLocksArn = String.format("arn:aws:dynamodb:%s:%s:table/%s", region, accountId,
-                dynamodbDistributedLocksTableName);
-        var distributedLocksTable = Table.fromTableArn(this, "DistributedLocksTable", distributedLocksArn);
+                // 构建分布式锁表的 ARN（包含账户 ID）
+                String distributedLocksArn = String.format("arn:aws:dynamodb:%s:%s:table/%s", region, accountId,
+                                dynamodbDistributedLocksTableName);
+                var distributedLocksTable = Table.fromTableArn(this, "DistributedLocksTable", distributedLocksArn);
 
-        String dynamodbSingleTableArn = String.format("arn:aws:dynamodb:%s:%s:table/%s", region, accountId,
-                dynamodbSingleTableName);
-        var authSingleTable = Table.fromTableArn(this, "SingleTable", dynamodbSingleTableArn);
+                String dynamodbSingleTableArn = String.format("arn:aws:dynamodb:%s:%s:table/%s", region, accountId,
+                                dynamodbSingleTableName);
+                var authSingleTable = Table.fromTableArn(this, "SingleTable", dynamodbSingleTableArn);
 
-        // 授予 Lambda 函数对 DynamoDB 表的读写权限
-        authSingleTable.grantReadWriteData(function);
-        distributedLocksTable.grantReadWriteData(function);
+                // 授予 Lambda 函数对 DynamoDB 表的读写权限
+                authSingleTable.grantReadWriteData(function);
+                distributedLocksTable.grantReadWriteData(function);
 
-        // 额外授予 Lambda 函数创建和管理索引的权限
-        function.addToRolePolicy(PolicyStatement.Builder.create().effect(Effect.ALLOW)
-                .actions(Arrays.asList("dynamodb:UpdateTable", "dynamodb:DescribeTable", "dynamodb:CreateTable"))
-                .resources(Arrays.asList(authSingleTable.getTableArn(),
-                        // index
-                        authSingleTable.getTableArn() + "/*", distributedLocksArn))
-                .build());
-        function.addToRolePolicy(PolicyStatement.Builder.create().effect(Effect.ALLOW)
-                .actions(Arrays.asList("dynamodb:Query", "dynamodb:Scan", "dynamodb:GetItem", "dynamodb:PutItem",
-                        "dynamodb:UpdateItem", "dynamodb:DeleteItem"))
-                .resources(Arrays.asList(authSingleTable.getTableArn(),
-                        // index
-                        authSingleTable.getTableArn() + "/*", distributedLocksArn))
-                .build());
+                // 额外授予 Lambda 函数创建和管理索引的权限
+                function.addToRolePolicy(PolicyStatement.Builder.create().effect(Effect.ALLOW)
+                                .actions(Arrays.asList("dynamodb:UpdateTable", "dynamodb:DescribeTable",
+                                                "dynamodb:CreateTable"))
+                                .resources(Arrays.asList(authSingleTable.getTableArn(),
+                                                // index
+                                                authSingleTable.getTableArn() + "/*", distributedLocksArn))
+                                .build());
+                function.addToRolePolicy(PolicyStatement.Builder.create().effect(Effect.ALLOW)
+                                .actions(Arrays.asList("dynamodb:Query", "dynamodb:Scan", "dynamodb:GetItem",
+                                                "dynamodb:PutItem", "dynamodb:UpdateItem", "dynamodb:DeleteItem"))
+                                .resources(Arrays.asList(authSingleTable.getTableArn(),
+                                                // index
+                                                authSingleTable.getTableArn() + "/*", distributedLocksArn))
+                                .build());
 
-        var currentVersion = function.getCurrentVersion();
-        var prodAlias = Alias.Builder.create(this, "ProdAlias").aliasName("Prod").version(currentVersion).build();
+                String ssmParameterArn = String.format("arn:aws:ssm:%s:%s:parameter/auth/resend/api-key", region,
+                                accountId);
 
-        HttpLambdaIntegration lambdaIntegration = HttpLambdaIntegration.Builder.create("LambdaIntegration", prodAlias)
-                .build();
+                // 添加 SSM Parameter Store 访问权限
+                function.addToRolePolicy(PolicyStatement.Builder.create().effect(Effect.ALLOW)
+                                .actions(Arrays.asList("ssm:GetParameter")).resources(Arrays.asList(ssmParameterArn))
+                                .build());
 
-        HttpApi httpApi = HttpApi.Builder.create(this, serviceName + "-http-api").defaultIntegration(lambdaIntegration)
-                .corsPreflight(CorsPreflightOptions.builder()
-                        .allowOrigins(Arrays.asList("https://sunbath.top", "http://localhost:4200"))
-                        .allowMethods(Arrays.asList(CorsHttpMethod.GET, CorsHttpMethod.POST, CorsHttpMethod.PUT,
-                                CorsHttpMethod.DELETE, CorsHttpMethod.OPTIONS))
-                        .allowHeaders(Arrays.asList("Content-Type", "Authorization", "X-Amz-Date", "X-Api-Key"))
-                        .allowCredentials(true).maxAge(Duration.days(1)).build())
-                .build();
+                var currentVersion = function.getCurrentVersion();
+                var prodAlias = Alias.Builder.create(this, "ProdAlias").aliasName("Prod").version(currentVersion)
+                                .build();
 
-        // 配置自定义域名
-        var domainName = "api.sunbath.top";
-        var basePath = "auth";
-        var domainHostedZoneId = "ZL327KTPIQFUL";
-        var domainAlias = "d-she55i1zs4.execute-api.ap-southeast-1.amazonaws.com";
-        var url = "https://" + domainName + "/" + basePath;
+                HttpLambdaIntegration lambdaIntegration = HttpLambdaIntegration.Builder
+                                .create("LambdaIntegration", prodAlias).build();
 
-        var domainNameV2 = DomainName.fromDomainNameAttributes(this, "ApiDomainNameV2", DomainNameAttributes.builder()
-                .name(domainName).regionalHostedZoneId(domainHostedZoneId).regionalDomainName(domainAlias).build());
+                HttpApi httpApi = HttpApi.Builder.create(this, serviceName + "-http-api")
+                                .defaultIntegration(lambdaIntegration)
+                                .corsPreflight(CorsPreflightOptions.builder()
+                                                .allowOrigins(Arrays.asList("https://sunbath.top",
+                                                                "http://localhost:4200"))
+                                                .allowMethods(Arrays.asList(CorsHttpMethod.GET, CorsHttpMethod.POST,
+                                                                CorsHttpMethod.PUT, CorsHttpMethod.DELETE,
+                                                                CorsHttpMethod.OPTIONS))
+                                                .allowHeaders(Arrays.asList("Content-Type", "Authorization",
+                                                                "X-Amz-Date", "X-Api-Key"))
+                                                .allowCredentials(true).maxAge(Duration.days(1)).build())
+                                .build();
 
-        ApiMapping.Builder.create(this, serviceName + "-api-mapping").api(httpApi).domainName(domainNameV2)
-                .apiMappingKey(basePath).stage(httpApi.getDefaultStage()).build();
+                // 配置自定义域名
+                var domainName = "api.sunbath.top";
+                var basePath = "auth";
+                var domainHostedZoneId = "ZL327KTPIQFUL";
+                var domainAlias = "d-she55i1zs4.execute-api.ap-southeast-1.amazonaws.com";
+                var url = "https://" + domainName + "/" + basePath;
 
-        // 输出 DynamoDB 表名
-        CfnOutput.Builder.create(this, "SingleTableName").exportName("SingleTableName")
-                .value(authSingleTable.getTableName()).build();
+                var domainNameV2 = DomainName.fromDomainNameAttributes(this, "ApiDomainNameV2",
+                                DomainNameAttributes.builder().name(domainName).regionalHostedZoneId(domainHostedZoneId)
+                                                .regionalDomainName(domainAlias).build());
 
-        CfnOutput.Builder.create(this, "AuthApiUrl").exportName("AuthApiUrl").value(url).build();
-    }
+                ApiMapping.Builder.create(this, serviceName + "-api-mapping").api(httpApi).domainName(domainNameV2)
+                                .apiMappingKey(basePath).stage(httpApi.getDefaultStage()).build();
 
-    public static String functionPath() {
-        return "../app/build/libs/" + functionFilename();
-    }
+                // 输出 DynamoDB 表名
+                CfnOutput.Builder.create(this, "SingleTableName").exportName("SingleTableName")
+                                .value(authSingleTable.getTableName()).build();
 
-    public static String functionFilename() {
-        return MicronautFunctionFile.builder().optimized().graalVMNative(false).version("0.1").archiveBaseName("app")
-                .buildTool(BuildTool.GRADLE).build();
-    }
+                CfnOutput.Builder.create(this, "AuthApiUrl").exportName("AuthApiUrl").value(url).build();
+        }
+
+        public static String functionPath() {
+                return "../app/build/libs/" + functionFilename();
+        }
+
+        public static String functionFilename() {
+                return MicronautFunctionFile.builder().optimized().graalVMNative(false).version("0.1")
+                                .archiveBaseName("app").buildTool(BuildTool.GRADLE).build();
+        }
 }

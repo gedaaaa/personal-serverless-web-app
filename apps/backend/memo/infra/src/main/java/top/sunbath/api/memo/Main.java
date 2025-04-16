4e12c11d55a6083ed9c8b3d3ccfa210b07ee24d7
Depeng Meng <buaa38270206@gmail.com>
2025-04-16 07:49:48 +0000

    refactor(memo): update main class and configuration for memo microservice

--- apps/backend/memo/infra/src/main/java/top/sunbath/api/memo/Main.java
+++ apps/backend/memo/infra/src/main/java/top/sunbath/api/memo/Main.java
@@ -1,11 +1,11 @@
-package top.sunbath.api.auth;
+package top.sunbath.api.memo;
 
 import software.amazon.awscdk.App;
 
 public class Main {
     public static void main(final String[] args) {
         App app = new App();
-        new AppStack(app, "AuthStack");
+        new AppStack(app, "MemoStack");
         app.synth();
     }
-} 
+}
--- apps/backend/memo/infra/src/test/java/top/sunbath/api/auth/AppStackTest.java
+++ apps/backend/memo/infra/src/test/java/top/sunbath/api/auth/AppStackTest.java
@@ -1,8 +1,10 @@
-package top.sunbath.api.auth;
+package top.sunbath.api.memo;
 
 import org.junit.jupiter.api.Test;
 import software.amazon.awscdk.App;
 import software.amazon.awscdk.assertions.Template;
+import top.sunbath.api.memo.AppStack;
+
 import java.io.File;
 import java.util.Collections;
 
@@ -11,9 +13,10 @@ class AppStackTest {
     @Test
     void testAppStack() {
         if (new File(AppStack.functionPath()).exists()) {
-            AppStack stack = new AppStack(new App(), "TestAuthAppStack");
+            AppStack stack = new AppStack(new App(), "TestMemoAppStack");
             Template template = Template.fromStack(stack);
-            template.hasResourceProperties("AWS::Lambda::Function", Collections.singletonMap("Handler", "io.micronaut.function.aws.proxy.payload2.APIGatewayV2HTTPEventFunction"));
+            template.hasResourceProperties("AWS::Lambda::Function", Collections.singletonMap("Handler",
+                    "io.micronaut.function.aws.proxy.payload2.APIGatewayV2HTTPEventFunction"));
         }
     }
-} 
+}

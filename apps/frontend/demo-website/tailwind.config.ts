2cde647c25200f14a1c410b3a9cb20f8d6d3ad4e
gedaaaa <buaa38270206@gmail.com>
2025-04-21 12:59:13 +0000

    feat(frontend): demo for css trick

--- apps/frontend/demo-website/tailwind.config.ts
+++ apps/frontend/demo-website/tailwind.config.ts
@@ -2,8 +2,7 @@ import typography from '@tailwindcss/typography';
 import type { Config } from 'tailwindcss';
 
 export default {
-  content: ['./src/**/*.{html,js,svelte,ts}'],
-
+  content: ['./src/**/*.{html,js,svelte,ts,svx}'],
   theme: {
     extend: {},
   },

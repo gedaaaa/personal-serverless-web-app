3052b076b1cd1280960ffd62bcd2dd2bc67c5f54
gedaaaa <buaa38270206@gmail.com>
2025-04-29 07:58:40 +0000

    fix(md): properly enable prerendering.

--- apps/frontend/demo-website/src/routes/playground/[slug]/+page.svelte
+++ apps/frontend/demo-website/src/routes/playground/[slug]/+page.svelte
@@ -1,34 +1,20 @@
 <script lang="ts">
-  import { onMount } from 'svelte';
   import type { Component } from 'svelte';
-  import { page } from '$app/state';
 
-  let PostComponent: Component;
-  let error: Error | null = null;
-
-  onMount(async () => {
-    try {
-      const slug = page.params.slug;
-      let module;
-      try {
-        module = await import(`../markdowns/${slug}.svx`);
-      } catch {
-        module = await import(`../markdowns/${slug}.md`);
-      }
-
-      PostComponent = module?.default as Component;
-    } catch (e) {
-      error = e as Error;
-    }
-  });
+  export let data: {
+    Content?: Component;
+    metadata: {
+      title: string;
+      date: string;
+      description: string;
+      tags: string[];
+    };
+  };
+  const Content = data.Content;
 </script>
 
-{#if error && import.meta.env.MODE === 'development'}
-  <p>Error: {error.message}</p>
-{/if}
-
-{#if PostComponent}
-  <PostComponent />
+{#if Content}
+  <Content />
 {:else}
-  <p>Loding...</p>
+  <p>Loading...</p>
 {/if}
